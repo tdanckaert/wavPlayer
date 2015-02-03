11d30880292656da68c4291e40d3443b1f371c9f
Thomas Danckaert <thomas.danckaert@gmail.com>
2015-02-03 10:52:45 +0100

    move vector of samples in Wave constructor.

--- wave.h
+++ wave.h
@@ -9,7 +9,7 @@ class Wave {
   
  public:
  Wave(std::vector<float> samples, unsigned int channels, unsigned int samplerate) :
-  channels(channels), samplerate(samplerate), samples(samples) {};
+   channels(channels), samplerate(samplerate), samples(std::move(samples) ) {};
   
   static Wave openSoundFile(const QString& fileName);
   
